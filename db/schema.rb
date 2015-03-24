d04faa51886c94899ed94f7c68a9beec82246b8a
leverich1991 <leverich@k-state.edu>
2015-03-24 10:00:06 -0500

    "3-24-15"

--- db/schema.rb
+++ db/schema.rb
@@ -34,11 +34,14 @@ ActiveRecord::Schema.define(version: 20150303232235) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.text     "password_digest"
-    t.integer  "points"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "name"
+    t.string   "email"
+    t.string   "uid"
+    t.string   "oauth_token"
+    t.string   "oauth_secret"
+    t.string   "provider"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
